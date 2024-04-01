8c386ca0df0cb9e5727a4390a2773f48bd0bb027
Muhammed Ershad P <muhammedershadp@gmail.com>
2024-04-01 21:48:55 +0530

    Built ui components sidebar, navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import MobileNav from './MobileNav'
+
+const Navbar = () => {
+  return (
+    <nav className='flex flex-between fixed z-50 w-full bg-dark-1 px-6 lg:px-10'>
+        <Link href='/' className='flex items-center gap-1'>
+            <Image src={'/icons/logo.svg'} width={32} height={32} alt='logo' className='max-sm:size-10' />
+            <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Zoom</p>
+        </Link>
+        <div className='flex-between gap-5'>
+            {/* {clerk user management} */}
+
+            <MobileNav />
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
